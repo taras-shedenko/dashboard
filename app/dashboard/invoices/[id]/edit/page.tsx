e5975630ab865425ff0650acf85e4815bbb1f7b1
Taras Shedenko <taras.shedenko@gmail.com>
2025-07-10 17:29:35 +0300

    Mutating Data

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,24 @@
+import Form from "@/app/ui/invoices/edit-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const invoice = await fetchInvoiceById(params.id);
+  const customers = await fetchCustomers();
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${params.id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
